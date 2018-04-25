25cae4a62e3854755050f1f9c552bd185870d7a4
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2018-04-25 16:51:23 +0800

    new skeleton

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using NoSQLinCSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IService1 service1 = new Service1();
+            Console.Out.WriteLineAsync(service1.GetData(10)).Wait();
+        }
+    }
+}
